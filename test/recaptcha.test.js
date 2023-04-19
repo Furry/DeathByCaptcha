8de2590adec770f5bb966b418053e99ec63e94b5
Furry <ender@thefluegges.com>
2023-04-18 23:23:20 -0400

    Added recaptcha v3 test

--- test/recaptcha.test.js
+++ test/recaptcha.test.js
@@ -6,10 +6,19 @@ import chai from "chai";
 dotenv.config();
 const { expect } = chai;
 
-describe("RecaptchaV2 via https://patrickhlauke.github.io/recaptcha/", () => {
+describe("Recaptcha Solves", () => {
     const solver = new DBC.Solver(process.env.AUTH_TOKEN);
-    it("Should return a proper solved captcha", async () => {
+    it("Should return a recaptchav2 solution via https://patrickhlauke.github.io/recaptcha/", async () => {
         const response = await solver.recaptchaV2("6Ld2sf4SAAAAAKSgzs0Q13IZhY02Pyo31S2jgOB5", "https://patrickhlauke.github.io/recaptcha/");
         expect(response.text.length).to.be.greaterThan(64);
     });
+
+    it("Should return a recaptchav3 solution via https://2captcha.com/demo/recaptcha-v3", async () => {
+        const response = await solver.recaptchaV3("6LfB5_IbAAAAAMCtsjEHEHKqcB9iQocwwxTiihJu", "https://2captcha.com/demo/recaptcha-v3", {
+            action: "demo_action",
+            min_score: 0.3
+        });
+        expect(response.text.length).to.be.greaterThan(64);
+    });
 })
+
